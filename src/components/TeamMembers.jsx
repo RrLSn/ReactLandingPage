2d9742fbb0d4c4125e7a569dff4ee157abe1f399
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-01-26 10:39:59 +0100

    Fixed

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -4,62 +4,62 @@ const TeamMembers = () => {
   return (
     <div className='teamMembers'>
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar10.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>FullStack Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar9.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>FullStack Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar8.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>Backend Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar7.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>Frontend Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar6.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>FullStack Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar5.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>Frontend Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar4.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>Backend Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar3.svg" alt="" />
+            <h4>Team Mate</h4>
+            <p>Frontend Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
-            <p>Caption</p>
+            <img className='img' src="/media/Avatar2.svg" alt="" />
+            <h4>Team Head</h4>
+            <p>Frontend Developer</p>
             </div>
 
             <div>
-            <img className='img' src="/media/img9.svg" alt="" />
-            <h4>Header</h4>
+            <img className='img' src="/media/Avatar1.svg" alt="" />
+            <h4>Team Mate</h4>
             <p>Caption</p>
             </div>
           </div>
